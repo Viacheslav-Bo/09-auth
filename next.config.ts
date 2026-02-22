979681b3035ee8979744b344c0b0462e578302d0
Viacheslav-Bo <zotikivna@gmail.com>
2026-02-22 16:00:16 +0200

    init

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -6,3 +6,5 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+// Для коректної роботи з віддаленими зображеннями у Next.js (аватар профілю) потрібно в next.config.ts додати розділ images з масивом remotePatterns, який обов’язково містить hostname: 'ac.goit.global'.
