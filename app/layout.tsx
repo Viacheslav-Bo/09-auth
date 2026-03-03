b69e2e42fc7ec78adc727e6999413c599d57cfec
Viacheslav-Bo <zotikivna@gmail.com>
2026-03-03 21:00:29 +0200

    way-way

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,8 +50,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  modal,
 }: Readonly<{
   children: React.ReactNode;
+  modal: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -62,6 +64,7 @@ export default function RootLayout({
           <AuthProvider>
             <Header />
             {children}
+            {modal}
             <Footer />
           </AuthProvider>
         </TanStackProvider>
